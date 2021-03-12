868d93de3c8d31f4dc5fce2bec42800f2e5616ac
danjwalton <20973540+danjwalton@users.noreply.github.com>
2021-03-12 16:46:57 +0000

    Legends and labels

--- charts.js
+++ charts.js
@@ -11,6 +11,8 @@ function draw_area_figure1(data, chart_id, margin, width, height,chart_config){
 
 var groups = ["africa","asia","europe","north_america","south_america"];
 
+var labels = ["Africa", "Asia", "Europe", "North America", "South America"];
+
 var dateParse = d3.timeParse("%d/%m/%Y");
 data.forEach(function(d){d.date = dateParse(d.date);
 });
@@ -21,7 +23,7 @@ var sumstat = d3.nest()
 
 var stackedData = d3.stack()
     .keys(groups)
-    .value(function(d,key){return d.values[0][key]})(sumstat);
+    .value(function(d,key){return d.values[0][key]})(sumstat)
 
 var svg = d3.select("#"+chart_id)
     .append("svg")
@@ -33,6 +35,8 @@ var svg = d3.select("#"+chart_id)
 var color = d3.scaleOrdinal()
 .domain(groups)
 .range([pal.orange1,pal.orange2,pal.orange3,pal.orange4,pal.orange5]);
+
+var label = function(d){return labels[groups.findIndex(group => group === d.key)]};
 
 var x = d3.scaleTime()
     .domain(d3.extent(data, function(d) { return d.date; }))
@@ -61,7 +65,7 @@ svg.append("text")
     .style("text-anchor", "middle")
     .text("Regional COVID-19 cases (millions)");
 
-var day_month_format = d3.timeFormat("%d %B");
+var day_month_format = d3.timeFormat("%d %b");
 
  // Add the area
  svg
@@ -88,7 +92,7 @@ var day_month_format = d3.timeFormat("%d %B");
         var closest_year = d.filter(function(i){return Math.abs(xPosition - i.x_pos) == closest_year_distance});
 
         tooltip.attr("transform", "translate(" + (xPosition + 10) + "," + yPosition + ")");
-        tooltip.select("text").text(d.key + ", "+closest_year[0].day_month+", "+((closest_year[0][1]-closest_year[0][0])/1000000).toFixed(2));
+        tooltip.select("text").text(label(d) + ", "+closest_year[0].day_month+", "+((closest_year[0][1]-closest_year[0][0])/1000000).toFixed(1) + "m");
     });
 
 var tooltip = svg.append("g")
@@ -107,7 +111,17 @@ tooltip.append("text")
     .style("text-anchor", "left")
     .attr("font-size", "12px")
     .attr("font-weight", "bold");
- 		  
+	
+svg.append("g")
+  .attr("class", "legendOrdinal")
+  .attr("transform", "translate(475,0)");
+
+var legend = d3.legendColor()
+    .scale(color)
+	.labels(labels);
+	
+svg.select(".legendOrdinal")
+  .call(legend);  		  
 }
 
 function draw_area_figure2a(data, chart_id, margin, width, height,chart_config){
@@ -166,14 +180,17 @@ svg.append("path")
     .on("mouseover", function() { tooltip.style("display", null); })
     .on("mouseout", function() { tooltip.style("display", "none"); })
     .on("mousemove", function(d) {
-        var xPosition = d3.mouse(this)[0]+5;
-        var yPosition = d3.mouse(this)[1];
-        var closest_year_distance = d3.min(d, function(i){console.log(i);return Math.abs(xPosition - i.data.values[0].date)});
-        console.log(closest_year_distance);
-        var closest_year = stackedData[0].filter(function(d){return Math.abs(xPosition - d.data.values[0].date) == closest_year_distance})[0].data.values[0].date;
-
-        tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-        tooltip.select("text").text(d.key + ", " + tooltip_formatter(d))}); 
+        var xPositionhold = d3.mouse(this)[0];
+        var yPositionhold = d3.mouse(this)[1];
+        var xPosition = x.invert(d3.mouse(this)[0]);
+        var yPosition = y.invert(d3.mouse(this)[1]);
+        console.log(xPosition);
+        var closest_year_distance = d3.min(data, function(d){ return Math.abs(xPosition - d.dd)});
+        var closest_year = data.filter(function(d){return Math.abs(xPosition - d.dd) == closest_year_distance})[0].day_month;
+        var closest_value = data.filter(function(d){return Math.abs(xPosition - d.dd) == closest_year_distance})[0].covid_cases_m;
+        console.log(d);
+        tooltip.attr("transform", "translate(" + (xPositionhold+10) + "," + yPositionhold + ")");
+        tooltip.select("text").text(closest_year+", "+closest_value+"m")});
 
 var tooltip = svg.append("g")
     .attr("class", "tooltip")
@@ -197,6 +214,7 @@ tooltip.append("text")
 function draw_area_figure2b(data, chart_id, margin, width, height,chart_config){
 
 var groups = ["all_paid","all_committed"];
+var labels = ["Paid", "Committed"];
 
 var dateParse = d3.timeParse("%d/%m/%Y");
 data.forEach(function(d){d.date = dateParse(d.date);
@@ -221,6 +239,8 @@ var color = d3.scaleOrdinal()
 .domain(groups)
 .range([pal.orange1,pal.orange2]);
 
+var label = function(d){return labels[groups.findIndex(group => group === d.key)]};
+
 var x = d3.scaleTime()
     .domain(d3.extent(data, function(d) { return d.date; }))
     .range([ 0, width ]).nice();
@@ -229,6 +249,8 @@ var x = d3.scaleTime()
 var y = d3.scaleLinear()
   .domain([0, d3.max(data, function(d) { return +d.all_total/1000000000; })])
   .range([ height, 0 ]).nice();
+  
+var day_month_format = d3.timeFormat("%d %b");
 
 svg.append("g")
     .attr("class", "axis")
@@ -260,92 +282,21 @@ svg.append("text")
         .y0(function(d) { return y(d[0]/1000000000); })
         .y1(function(d) { return y(d[1]/1000000000); })
     )
-}
-
-function draw_area_figure8(data, chart_id, margin, width, height,chart_config){
-
-var groups = ["governments","ngos","red_cross","who","un","uncategorised"];
-
-var dateParse = d3.timeParse("%d/%m/%Y");
-data.forEach(function(d){d.date = dateParse(d.date);
-});
-
-var sumstat = d3.nest()
-.key(function(d) { return d.date;})
-.entries(data);
-
-
-
-var stackedData = d3.stack()
-    .keys(groups)
-    .value(function(d,key){return d.values[0][key]})(sumstat);
-
-var svg = d3.select("#"+chart_id)
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var color = d3.scaleOrdinal()
-.domain(groups)
-.range([pal.orange1,pal.orange2,pal.orange3,pal.orange4,pal.orange5,pal.grey1]);
-
-var x = d3.scaleTime()
-    .domain(d3.extent(data, function(d) { return d.date; }))
-    .range([ 0, width ]).nice();
-
-// Add Y axis
-var y = d3.scaleLinear()
-  .domain([0, 100])
-  .range([ height, 0 ]).nice();
-
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%b")));
-
-svg.append("g")
-    .attr("class", "axis")  
-    .call(d3.axisLeft(y));
-	
-svg.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0-margin.left)
-    .attr("x",0 - (height / 1.8))
-    .attr("dy", "1em")
-    .attr('class','yaxistitle')
-    .style("text-anchor", "middle")
-    .text("Share of COVID-19 funding received (%)");
-
- // Add the area
- svg
-    .selectAll("mylayers")
-    .data(stackedData)
-    .enter()
-    .append("path")
-      .style("fill", function(d) {return color(d.key)})
-      .attr("d", d3.area()
-        .x(function(d) { return x(d.data.values[0].date); })
-        .y0(function(d) { return y(d[0]*100); })
-        .y1(function(d) { return y(d[1]*100); })
-    )
     .on("mouseover", function() { tooltip.style("display", null); })
     .on("mouseout", function() { tooltip.style("display", "none"); })
     .on("mousemove", function(d) {
-        var xPositionhold = d3.mouse(this)[0];
-        var yPositionhold = d3.mouse(this)[1];
-        var xPosition = x.invert(d3.mouse(this)[0]);
-        console.log(xPosition);
-        var yPosition = y.invert(d3.mouse(this)[1]);
-        var closest_year_distance = d3.min(stackedData, function(d,d_index){return Math.abs(xPosition - d[d_index].data.values[0].date)});
-        console.log(stackedData[0]);
-        var closest_year = stackedData[0].filter(function(d){return Math.abs(xPosition - d.data.values[0].date) == closest_year_distance})[0].data.values[0].date;
-        console.log(closest_year);
-        var closest_value = stackedData.filter(function(d,d_index){return Math.abs(xPosition - d[d_index].data.values[0].date) == closest_year_distance});
-        tooltip.attr("transform", "translate(" + (xPositionhold+10) + "," + yPositionhold + ")");
-        // tooltip.select("text").text(closest_year+", "+closest_value+"m")
-      });
+        var xPosition = d3.mouse(this)[0];
+        var yPosition = d3.mouse(this)[1];
+        d.forEach(function(d){d.x_pos = x(d.data.values[0].date);
+        d.day_month = day_month_format(d.data.values[0].date)});
+
+        var closest_year_distance = d3.min(d, function(i){return Math.abs(xPosition - i.x_pos)});
+   
+        var closest_year = d.filter(function(i){return Math.abs(xPosition - i.x_pos) == closest_year_distance});
+
+        tooltip.attr("transform", "translate(" + (xPosition + 10) + "," + yPosition + ")");
+        tooltip.select("text").text(label(d) + ", "+closest_year[0].day_month+", US$"+((closest_year[0][1]-closest_year[0][0])/1000000000).toFixed(1) + "bn");
+    });
 
 var tooltip = svg.append("g")
     .attr("class", "tooltip")
@@ -364,4 +315,131 @@ tooltip.append("text")
     .attr("font-size", "12px")
     .attr("font-weight", "bold");
  		  
+svg.append("g")
+  .attr("class", "legendOrdinal")
+  .attr("transform", "translate(475,0)");
+
+var legend = d3.legendColor()
+    .scale(color)
+	.labels(labels);
+	
+svg.select(".legendOrdinal")
+  .call(legend); 
+}
+
+function draw_area_figure8(data, chart_id, margin, width, height,chart_config){
+
+var groups = ["governments","ngos","red_cross","who","un","uncategorised"];
+var labels = ["Governments","NGOs","Red Cross","WHO","UN","Uncategorised"];
+
+var dateParse = d3.timeParse("%d/%m/%Y");
+data.forEach(function(d){d.date = dateParse(d.date);
+});
+
+var sumstat = d3.nest()
+.key(function(d) { return d.date;})
+.entries(data);
+
+var stackedData = d3.stack()
+    .keys(groups)
+    .value(function(d,key){return d.values[0][key]})(sumstat);
+
+var svg = d3.select("#"+chart_id)
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var color = d3.scaleOrdinal()
+	.domain(groups)
+	.range([pal.orange1,pal.orange2,pal.orange3,pal.orange4,pal.orange5,pal.grey1]);
+
+var label = function(d){return labels[groups.findIndex(group => group === d.key)]};
+
+var x = d3.scaleTime()
+    .domain(d3.extent(data, function(d) { return d.date; }))
+    .range([ 0, width ]).nice();
+
+// Add Y axis
+var y = d3.scaleLinear()
+  .domain([0, 100])
+  .range([ height, 0 ]).nice();
+ 
+var day_month_format = d3.timeFormat("%d %b");
+
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%b")));
+
+svg.append("g")
+    .attr("class", "axis")  
+    .call(d3.axisLeft(y));
+	
+svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0-margin.left)
+    .attr("x",0 - (height / 1.8))
+    .attr("dy", "1em")
+    .attr('class','yaxistitle')
+    .style("text-anchor", "middle")
+    .text("Share of COVID-19 funding received (%)");		
+
+ // Add the area
+ svg
+    .selectAll("mylayers")
+    .data(stackedData)
+    .enter()
+    .append("path")
+      .style("fill", function(d) {return color(d.key)})
+      .attr("d", d3.area()
+        .x(function(d) { return x(d.data.values[0].date); })
+        .y0(function(d) { return y(d[0]*100); })
+        .y1(function(d) { return y(d[1]*100); })
+    )
+    .on("mouseover", function() { tooltip.style("display", null); })
+    .on("mouseout", function() { tooltip.style("display", "none"); })
+    .on("mousemove", function(d) {
+        var xPosition = d3.mouse(this)[0];
+        var yPosition = d3.mouse(this)[1];
+        d.forEach(function(d){d.x_pos = x(d.data.values[0].date);
+        d.day_month = day_month_format(d.data.values[0].date)});
+
+        var closest_year_distance = d3.min(d, function(i){return Math.abs(xPosition - i.x_pos)});
+   
+        var closest_year = d.filter(function(i){return Math.abs(xPosition - i.x_pos) == closest_year_distance});
+
+        tooltip.attr("transform", "translate(" + (xPosition + 10) + "," + yPosition + ")");
+        tooltip.select("text").text(label(d) + ", "+closest_year[0].day_month+", "+((closest_year[0][1]-closest_year[0][0])*100).toFixed(1) + "%");
+    });
+
+var tooltip = svg.append("g")
+    .attr("class", "tooltip")
+    .style("display", "none");   
+        
+tooltip.append("rect")
+    .attr("width", 100)
+    .attr("height", 20)
+    .attr("fill", "white")
+    .style("opacity", 0);
+  
+tooltip.append("text")
+    .attr("x", 2)
+    .attr("dy", "1.2em")
+    .style("text-anchor", "left")
+    .attr("font-size", "12px")
+    .attr("font-weight", "bold");  
+
+svg.append("g")
+  .attr("class", "legendOrdinal")
+  .attr("transform", "translate(475,0)");
+
+var legend = d3.legendColor()
+    .scale(color)
+	.labels(labels);
+	
+svg.select(".legendOrdinal")
+  .call(legend); 
+
 }
