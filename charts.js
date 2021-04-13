04cfd64d9ee31aa5c18cbffab7c8d209895a6099
danjwalton <20973540+danjwalton@users.noreply.github.com>
2021-04-13 17:15:27 +0100

    SVG tooltips

--- charts.js
+++ charts.js
@@ -88,25 +88,37 @@ var day_month_format = d3.timeFormat("%d %b");
         .y0(function(d) { return y(d[0]/1000000); })
         .y1(function(d) { return y(d[1]/1000000); })
     )
-    .on("mouseover", function() { tooltip.style("display", null); })
-    .on("mouseout", function() { tooltip.style("display", "none"); })
+    .on("mouseover", function() { tooltip.style("display", null); tooltipBackground.style("display", null);})
+    .on("mouseout", function() { tooltip.style("display", "none"); tooltipBackground.style("display", "none"); })
     .on("mousemove", function(d) {
-        var xPosition = d3.mouse(this)[0];
+        var xPosition = d3.mouse(this)[0]+20;
         var yPosition = d3.mouse(this)[1];
 
         d.forEach(function(d){d.x_pos = x(d.data.values[0].date);
         d.day_month = day_month_format(d.data.values[0].date)});
 
-		tooltip.style("left", d3.event.pageX+20+"px");
-        tooltip.style("top", d3.event.pageY-25+"px");
-        tooltip.style("display", "inline-block");
-		
 		var closest_year_distance = d3.min(d, function(i){return Math.abs(xPosition - i.x_pos)});
 		var closest_year = d.filter(function(i){return Math.abs(xPosition - i.x_pos) == closest_year_distance});
 		
-		tooltip.html(label(d)+"<br/>"+closest_year[0].day_month+"<br/>"+((closest_year[0][1]-closest_year[0][0])/1000000).toFixed(1) + "m");
+		tooltip.attr("x",xPosition).attr("y",yPosition);
+		tooltip_line0.attr("x", xPosition);
+        tooltip_line1.attr("x", xPosition);
+        tooltip_line2.attr("x", xPosition);
+        tooltip_line0.text(label(d));
+        tooltip_line1.text(closest_year[0].day_month);
+        tooltip_line2.text(((closest_year[0][1]-closest_year[0][0])/1000000).toFixed(1) + "m");
+		
+		var tooltip_bbox = tooltip.node().getBBox();
+          tooltipBackground
+          .attr("x",tooltip_bbox.x - 5)
+          .attr("y",tooltip_bbox.y - 5)
+          .attr("height", tooltip_bbox.height + 10)
+          .attr("width", tooltip_bbox.width + 10)
+          .style("opacity","1");
+		
+		
     });
-
+	
 svg.append("g")
   .attr("class", "legendOrdinal")
   .attr("transform", "translate(475,0)");
@@ -117,13 +129,34 @@ var legend = d3.legendColor()
 	.ascending("True");
 	
 svg.select(".legendOrdinal")
-  .call(legend);  
-	 
-var tooltip = d3.select("#"+chart_id)
-	.append("div")
-    .attr("class", "tooltip")
-	.style("display", "none");   
-	
+  .call(legend); 	
+  
+var tooltipBackground = svg.append("rect")
+	.attr("class","tooltip-bg")
+	.attr("fill","white")
+	.attr("stroke-width",0.1)
+	.attr("stroke","black");
+
+var tooltip = svg.append("text")
+	.attr("class", "tooltip")
+	.attr("x", 0)
+	.attr("y", 0)
+	.attr("dy", "1.2em")
+	.style("text-anchor", "left")
+	.attr("font-size", "12px")
+	.attr("font-weight", "normal")
+	.style("fill", "#443e42")
+	.style("display", "none");
+
+var tooltip_line0 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
+var tooltip_line1 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
+var tooltip_line2 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
 }
 
 function draw_area_figure2a(data, chart_id, margin, width, height,chart_config){
@@ -170,22 +203,34 @@ svg.append("path")
       .y0(y(0))
       .y1(function(d) { return y(d.covid_cases_m) })
       )
-    .on("mouseover", function() { tooltip.style("display", null); })
-    .on("mouseout", function() { tooltip.style("display", "none"); })
+    .on("mouseover", function() { tooltip.style("display", null); tooltipBackground.style("display", null);})
+    .on("mouseout", function() { tooltip.style("display", "none"); tooltipBackground.style("display", "none"); })
     .on("mousemove", function(d) {
-        var xPositionhold = d3.mouse(this)[0];
+        var xPositionhold = d3.mouse(this)[0]+20;
         var yPositionhold = d3.mouse(this)[1];
         var xPosition = x.invert(d3.mouse(this)[0]);
         var yPosition = y.invert(d3.mouse(this)[1]);
-        console.log(xPosition);
+
         var closest_year_distance = d3.min(data, function(d){ return Math.abs(xPosition - d.dd)});
         var closest_year = data.filter(function(d){return Math.abs(xPosition - d.dd) == closest_year_distance})[0].day_month;
         var closest_value = data.filter(function(d){return Math.abs(xPosition - d.dd) == closest_year_distance})[0].covid_cases_m;
         console.log(d);
-        tooltip.style("left", d3.event.pageX+20+"px");
-        tooltip.style("top", d3.event.pageY-25+"px");
-        tooltip.style("display", "inline-block");
-        tooltip.html(closest_year+"<br/>"+closest_value+"m")
+		
+		tooltip.attr("x",xPositionhold).attr("y",yPositionhold);
+		tooltip_line0.attr("x", xPositionhold);
+        tooltip_line1.attr("x", xPositionhold);
+		tooltip_line0.text(closest_year);
+		tooltip_line1.text(closest_value+"m");
+		
+		var tooltip_bbox = tooltip.node().getBBox();
+          tooltipBackground
+          .attr("x",tooltip_bbox.x - 5)
+          .attr("y",tooltip_bbox.y - 5)
+          .attr("height", tooltip_bbox.height + 10)
+          .attr("width", tooltip_bbox.width + 10)
+          .style("opacity","1");
+		
+		
 		});
 
 svg.append("g")
@@ -206,10 +251,29 @@ svg.append("text")
     .style("text-anchor", "middle")
     .text("Covid-19 cases (millions)");
 
-var tooltip = d3.select("#"+chart_id)
-	.append("div")
-    .attr("class", "tooltip")
-	.style("display", "none");    
+var tooltipBackground = svg.append("rect")
+	.attr("class","tooltip-bg")
+	.attr("fill","white")
+	.attr("stroke-width",0.1)
+	.attr("stroke","black");
+
+var tooltip = svg.append("text")
+	.attr("class", "tooltip")
+	.attr("x", 0)
+	.attr("y", 0)
+	.attr("dy", "1.2em")
+	.style("text-anchor", "left")
+	.attr("font-size", "12px")
+	.attr("font-weight", "normal")
+	.style("fill", "#443e42")
+	.style("display", "none");
+
+var tooltip_line0 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
+var tooltip_line1 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
 
 }
 
@@ -293,29 +357,37 @@ svg.append("text")
         .y0(function(d) { return y(d[0]/1000000000); })
         .y1(function(d) { return y(d[1]/1000000000); })
     )
-    .on("mouseover", function() { tooltip.style("display", null); })
-    .on("mouseout", function() { tooltip.style("display", "none"); })
+    .on("mouseover", function() { tooltip.style("display", null); tooltipBackground.style("display", null);})
+    .on("mouseout", function() { tooltip.style("display", "none"); tooltipBackground.style("display", "none"); })
     .on("mousemove", function(d) {
-        var xPosition = d3.mouse(this)[0];
+        var xPosition = d3.mouse(this)[0]+20;
         var yPosition = d3.mouse(this)[1];
+
         d.forEach(function(d){d.x_pos = x(d.data.values[0].date);
         d.day_month = day_month_format(d.data.values[0].date)});
 
-        var closest_year_distance = d3.min(d, function(i){return Math.abs(xPosition - i.x_pos)});
-   
-        var closest_year = d.filter(function(i){return Math.abs(xPosition - i.x_pos) == closest_year_distance});
-
-        tooltip.style("left", d3.event.pageX+20+"px");
-        tooltip.style("top", d3.event.pageY-25+"px");
-        tooltip.style("display", "inline-block");
-        tooltip.html(label(d) + "<br/>"+closest_year[0].day_month+"<br/>US$"+((closest_year[0][1]-closest_year[0][0])/1000000000).toFixed(1) + "bn");
+		var closest_year_distance = d3.min(d, function(i){return Math.abs(xPosition - i.x_pos)});
+		var closest_year = d.filter(function(i){return Math.abs(xPosition - i.x_pos) == closest_year_distance});
+		
+		tooltip.attr("x",xPosition).attr("y",yPosition);
+		tooltip_line0.attr("x", xPosition);
+        tooltip_line1.attr("x", xPosition);
+        tooltip_line2.attr("x", xPosition);
+        tooltip_line0.text(label(d));
+        tooltip_line1.text(closest_year[0].day_month);
+        tooltip_line2.text("US$"+((closest_year[0][1]-closest_year[0][0])/1000000000).toFixed(1) + "bn");
+		
+		var tooltip_bbox = tooltip.node().getBBox();
+          tooltipBackground
+          .attr("x",tooltip_bbox.x - 5)
+          .attr("y",tooltip_bbox.y - 5)
+          .attr("height", tooltip_bbox.height + 10)
+          .attr("width", tooltip_bbox.width + 10)
+          .style("opacity","1");
+		
+		
     });
 
-var tooltip = d3.select("#"+chart_id)
-	.append("div")
-    .attr("class", "tooltip")
-	.style("display", "none");   
- 		  
 svg.append("g")
   .attr("class", "legendOrdinal")
   .attr("transform", "translate(475,0)");
@@ -327,6 +399,33 @@ var legend = d3.legendColor()
 	
 svg.select(".legendOrdinal")
   .call(legend); 
+  
+var tooltipBackground = svg.append("rect")
+	.attr("class","tooltip-bg")
+	.attr("fill","white")
+	.attr("stroke-width",0.1)
+	.attr("stroke","black");
+
+var tooltip = svg.append("text")
+	.attr("class", "tooltip")
+	.attr("x", 0)
+	.attr("y", 0)
+	.attr("dy", "1.2em")
+	.style("text-anchor", "left")
+	.attr("font-size", "12px")
+	.attr("font-weight", "normal")
+	.style("fill", "#443e42")
+	.style("display", "none");
+
+var tooltip_line0 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
+var tooltip_line1 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
+var tooltip_line2 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
 }
 
 function draw_area_figure8(data, chart_id, margin, width, height,chart_config){
@@ -409,29 +508,37 @@ svg.append("text")
         .y0(function(d) { return y(d[0]*100); })
         .y1(function(d) { return y(d[1]*100); })
     )
-    .on("mouseover", function() { tooltip.style("display", null); })
-    .on("mouseout", function() { tooltip.style("display", "none"); })
+    .on("mouseover", function() { tooltip.style("display", null); tooltipBackground.style("display", null);})
+    .on("mouseout", function() { tooltip.style("display", "none"); tooltipBackground.style("display", "none"); })
     .on("mousemove", function(d) {
-        var xPosition = d3.mouse(this)[0];
+        var xPosition = d3.mouse(this)[0]+20;
         var yPosition = d3.mouse(this)[1];
+
         d.forEach(function(d){d.x_pos = x(d.data.values[0].date);
         d.day_month = day_month_format(d.data.values[0].date)});
 
-        var closest_year_distance = d3.min(d, function(i){return Math.abs(xPosition - i.x_pos)});
-   
-        var closest_year = d.filter(function(i){return Math.abs(xPosition - i.x_pos) == closest_year_distance});
-
-        tooltip.style("left", d3.event.pageX+20+"px");
-        tooltip.style("top", d3.event.pageY-25+"px");
-        tooltip.style("display", "inline-block");
-        tooltip.html(label(d) + "<br/>"+closest_year[0].day_month+"<br/>"+((closest_year[0][1]-closest_year[0][0])*100).toFixed(1) + "%");
+		var closest_year_distance = d3.min(d, function(i){return Math.abs(xPosition - i.x_pos)});
+		var closest_year = d.filter(function(i){return Math.abs(xPosition - i.x_pos) == closest_year_distance});
+		
+		tooltip.attr("x",xPosition).attr("y",yPosition);
+		tooltip_line0.attr("x", xPosition);
+        tooltip_line1.attr("x", xPosition);
+        tooltip_line2.attr("x", xPosition);
+        tooltip_line0.text(label(d));
+        tooltip_line1.text(closest_year[0].day_month);
+        tooltip_line2.text(((closest_year[0][1]-closest_year[0][0])*100).toFixed(1) + "%");
+		
+		var tooltip_bbox = tooltip.node().getBBox();
+          tooltipBackground
+          .attr("x",tooltip_bbox.x - 5)
+          .attr("y",tooltip_bbox.y - 5)
+          .attr("height", tooltip_bbox.height + 10)
+          .attr("width", tooltip_bbox.width + 10)
+          .style("opacity","1");
+		
+		
     });
-
-var tooltip = d3.select("#"+chart_id)
-	.append("div")
-    .attr("class", "tooltip")
-	.style("display", "none");   
-
+	
 svg.append("g")
   .attr("class", "legendOrdinal")
   .attr("transform", "translate(475,0)");
@@ -443,5 +550,31 @@ var legend = d3.legendColor()
 	
 svg.select(".legendOrdinal")
   .call(legend); 
+	
+var tooltipBackground = svg.append("rect")
+	.attr("class","tooltip-bg")
+	.attr("fill","white")
+	.attr("stroke-width",0.1)
+	.attr("stroke","black");
 
+var tooltip = svg.append("text")
+	.attr("class", "tooltip")
+	.attr("x", 0)
+	.attr("y", 0)
+	.attr("dy", "1.2em")
+	.style("text-anchor", "left")
+	.attr("font-size", "12px")
+	.attr("font-weight", "normal")
+	.style("fill", "#443e42")
+	.style("display", "none");
+
+var tooltip_line0 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
+var tooltip_line1 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
+var tooltip_line2 = tooltip.append("tspan")
+	.attr("x", 0)
+	.attr("dy", "1.2em");
 }
